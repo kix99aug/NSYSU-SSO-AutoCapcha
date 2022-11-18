5446799f058172c611a964533fc49142a354a24a
Hanyu Huang <kix99aug@gmail.com>
2022-11-18 16:52:23 +0800

    Create NSYSU-SSO-AutoCapcha.user.js

---
+++ NSYSU-SSO-AutoCapcha.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         NSYSU SSO AutoCapcha
+// @namespace    https://keybo.space/
+// @version      1.0
+// @description  Get rid of capcha!
+// @author       Kix
+// @match        https://sso.nsysu.edu.tw/
+// @match        https://sso.nsysu.edu.tw/index.php/passport/login
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.nsysu.edu.tw
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+    var ans = ""
+    while(ans.length != 4){
+        var response = await fetch("https://sso.nsysu.edu.tw/views/index/showpic.php");
+        var content = await response.blob();
+        var formData = new FormData();
+        formData.append("file", content);
+        formData.append("filetype", "jpg");
+        formData.append("OCREngine", "2");
+        response = await fetch("https://api.ocr.space/parse/image",{
+            headers:{
+                "apikey":"K86777676288957"},
+            method:"POST",
+            body:formData
+        })
+        content = await response.json();
+        content = content.ParsedResults[0].ParsedText.toLowerCase()
+        let array = [...content.matchAll(/[a-z]/g)];
+        for(let i = 0;i<array.length;i++){
+            if(!array[i]) array.splice(i--,1)
+        }
+        ans = array.join("")
+    }
+    document.getElementById("anscheck").value = ans
+})();
